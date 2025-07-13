2bd3ad09046775e88e7481b5915646add4f6478c
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-07-13 16:04:10 +0100

    update

--- website/src/travel-map.js
+++ website/src/travel-map.js
@@ -3,10 +3,34 @@ import "jsvectormap/dist/jsvectormap.min.css"
 import "jsvectormap/dist/maps/world"
 import "./style.css"
 
+const places = [
+  // North America
+  "US", // United States
+
+  // Europe
+  "GB", // United Kingdom
+  "IT", // Italy
+  "FR", // France
+  "ES", // Spain
+  "PT", // Portugal
+  "CY", // Cyprus
+  "GR", // Greece
+  "TR", // Turkey
+
+  // Asia
+  "CN", // China
+  "SG", // Singapore
+
+  // Oceania
+  "AU", // Australia
+]
+
 new jsVectorMap({
   selector: "#map",
   map: "world",
   regionStyle: {
     selectedHover: { fill: "gray" },
+    selected: { fill: 'green' },
   },
+  selectedRegions: places,
 })
