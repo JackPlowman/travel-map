2a0a219ee3b9cf3f0ee3b2cdd57270fed6cbcebc
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-07-13 12:56:46 +0100

    Add JavaScript Linting and Formatting Jobs (#31)
    # Pull Request
    
    ## Description
    
    This pull request introduces JavaScript linting and formatting tools
    into the project, updates related workflows, and enhances code style
    consistency. The changes include new workflows for linting and
    formatting, configuration files for ESLint and Prettier, and updates to
    JavaScript files to align with the new formatting rules.
    
    ### Workflow Enhancements:
    * Added `run-javascript-lint-checks` and `run-javascript-format-checks`
    jobs to the `.github/workflows/code-checks.yml` file. These jobs set up
    Node.js, install dependencies, and run ESLint and Prettier checks,
    respectively.
    
    ### Tooling and Configuration:
    * Added a new ESLint configuration file (`website/eslint.config.js`) to
    enable linting with recommended settings and Prettier integration.
    * Added a Prettier configuration file (`website/prettier.config.js`) to
    define formatting rules, including support for organizing imports.
    * Updated `website/package.json` to include new dependencies for ESLint,
    Prettier, and related plugins.
    
    ### Code Style Updates:
    * Reformatted `website/src/travel-map.js` to comply with Prettier rules,
    including consistent use of double quotes and removal of unnecessary
    semicolons.
    * Adjusted the HTML structure in `website/index.html` to improve
    readability by formatting the `div` tag.

--- website/src/travel-map.js
+++ website/src/travel-map.js
@@ -1,12 +1,12 @@
-import "./style.css";
-import jsVectorMap from "jsvectormap";
-import "jsvectormap/dist/jsvectormap.min.css";
-import "jsvectormap/dist/maps/world";
+import jsVectorMap from "jsvectormap"
+import "jsvectormap/dist/jsvectormap.min.css"
+import "jsvectormap/dist/maps/world"
+import "./style.css"
 
-const map = new jsVectorMap({
+new jsVectorMap({
   selector: "#map",
   map: "world",
   regionStyle: {
-    selectedHover: { fill: 'gray' },
+    selectedHover: { fill: "gray" },
   },
-});
+})
