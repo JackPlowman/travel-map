6125303b3e1376f7617e43f20b90c022be7db263
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-11-02 14:26:13 +0000

    update

--- website/src/security.js
+++ website/src/security.js
@@ -1,10 +1,9 @@
 // Security enhancements for the web application
-(function() {
-  'use strict';
+;(function () {
+  "use strict"
 
   // Prevent clickjacking by ensuring the page is not loaded in a frame
   if (window.top !== window.self) {
-    window.top.location = window.self.location;
+    window.top.location = window.self.location
   }
-
-})();
+})()
